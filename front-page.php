96608782a6a6125cd35ab4de4be9c1aa23d5e79a
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-07-19 23:31:13 -0700

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -6,16 +6,19 @@ get_header();
     
     
     <div id="wrapper">
+        
+        <?php if(has_post_thumbnail()) : ?>
+        <?php the_post_thumbnail(); ?>
+        <?php endif; ?>
+        
         <!--if we have posts, show me the posts. If not, we don't-->
-        <?php if(have_posts()) : ?>
+        
         <?php while(have_posts()) : the_post(); ?>
         <h2><?php the_title(); ?></h2>
         <?php the_content(); ?> <!--the content lives in the wp posts in the wp dashboard-->
         <?php endwhile; ?>
-        <?php else : ?>
-        <?php echo wpautop('Sorry, no posts were found!'); ?>
-        <?php endif; ?>
-    <p>Here is my content</p>
+        
+    
     </div> <!--end wrapper-->
 
 <?php 
