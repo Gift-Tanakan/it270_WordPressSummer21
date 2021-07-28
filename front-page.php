116b39bb74c5c0a5cfe13e1d63e2b8e5b4ce90b9
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-07-27 22:23:39 -0700

    WK4 add div hero (banner) image

--- front-page.php
+++ front-page.php
@@ -3,9 +3,12 @@ get_header();
 
 /* Template Name: Front Page*/
 ?>
+<div id="hero">
+    <img src="<?php echo get_template_directory_uri(); ?>/../../uploads/2021/07/yellowstone-inner.jpg" alt="banner">
+</div> <!---end hero-->
     
     
-    <div id="wrapper">
+    <div class="wrapper">
         
         <?php if(has_post_thumbnail()) : ?>
         <?php the_post_thumbnail(); ?>
@@ -14,7 +17,7 @@ get_header();
         <!--if we have posts, show me the posts. If not, we don't-->
         
         <?php while(have_posts()) : the_post(); ?>
-        <h2><?php the_title(); ?></h2>
+       
         <?php the_content(); ?> <!--the content lives in the wp posts in the wp dashboard-->
         <?php endwhile; ?>
         
