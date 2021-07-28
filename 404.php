db15336fa93b70b5c228d00cfc8f86f71887ef18
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-07-27 22:32:54 -0700

    WK4 add banner

--- 404.php
+++ 404.php
@@ -2,9 +2,12 @@
 get_header();
 
 ?>
-
-<div id="wrapper">
-    <main>
+<div id="search-error">
+ <img src="<?php echo get_template_directory_uri(); ?>/../../uploads/searching-e1627363991432.jpg" alt="searching">
+    
+</div>
+<div class="wrapper">
+    <main class="search-inner">
         <h2><?php _e('Oh oh!', 'site-summer-1'); ?></h2>
         <p><?php _e('It looks like nothing was found at this location. Maybe try another search?', 'site-summer-1'); ?></p>
         
