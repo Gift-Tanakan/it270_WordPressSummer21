6d9a0265690163dd72c27d858554acf96b9b91fb
Gift-Tanakan <50960357+Gift-Tanakan@users.noreply.github.com>
2021-07-27 22:22:20 -0700

    WK4 add dynamic footer

--- footer.php
+++ footer.php
@@ -1,13 +1,4 @@
-<footer>
-    <div class="copyright">
-    <ul>
-        <li>Copyright <?php echo date('Y'); ?></li>
-        <li><a href="">Terms of use</a></li>
-        <li>Web design by Gift</li>
-        <li>All Rights Reserved</li>
-    </ul>
-    </div> <!--close copyright div-->
-</footer>
+<?php dynamic_sidebar('sidebar-footer-content'); ?>
 
 		<script>
 		$(document).ready(function(){
